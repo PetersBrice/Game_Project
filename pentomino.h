c9d17fa1073f18292c801b6707f1d85f2e7f0a5e
DecolleAnais <anais.decolle8@etu.univ-lorraine.fr>
2014-10-11 11:17:08 +0200

    print a shape

--- pentomino.h
+++ pentomino.h
@@ -25,6 +25,7 @@ void draw_pentomino(pentomino_ptr pentomino,SDL_Surface *square_sprite,SDL_Surfa
 pentomino_ptr mirror(pentomino_ptr pentomino);
 
 pentomino_ptr create(int x,int y,FILE *file);
+pentomino_ptr shape(int x, int y, FILE *file);
 
 
 #endif
--- square.c
+++ square.c
@@ -5,6 +5,7 @@ square_ptr new_square(SDL_Rect rcSrc,SDL_Rect rcSprite)
   square_ptr new_square = (square_ptr)malloc(sizeof(struct square));
   new_square->rcSrc = rcSrc;
   new_square->rcSprite = rcSprite;
+  return new_square;
 }
 
 void draw_square(square_ptr square,SDL_Surface *square_sprite,SDL_Surface *background)
@@ -14,5 +15,5 @@ void draw_square(square_ptr square,SDL_Surface *square_sprite,SDL_Surface *backg
   printf("x %d\n",pos_square.x);
   pos_square.y = square->rcSrc.y ; 
   printf("y %d\n",pos_square.x);
-  SDL_BlitSurface(square_sprite,NULL,background,&pos_square);  
+  SDL_BlitSurface(square_sprite,NULL,background,&pos_square);
 }
